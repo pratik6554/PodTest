73a34a7e2d539405b7058ed909a9fe853a2c1db5
PratikGandhi <pratik6554@gmail.com>
2017-06-16 11:25:45 +0530

    added framework files

---
+++ TestFrame/WCMathHelper.swift
@@ -0,0 +1,27 @@
+//
+//  WCMathHelper.swift
+//  TestFrame
+//
+//  Created by Pratik Gandhi on 15/06/17.
+//  Copyright © 2017 Pratik Gandhi. All rights reserved.
+//
+
+import Foundation
+
+public class WCMathHelper {
+
+ public init() {}
+
+  public func add(a :Int, b: Int) -> Int {
+     return a + b
+  }
+
+  func subtract(a :Int, b: Int) -> Int {
+    return a - b
+  }
+
+  func multiply(a :Int, b: Int) -> Int {
+    return a * b
+  }
+  
+}
